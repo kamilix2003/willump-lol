bd5d0ffdf8443ff735ed22e896b70f114c194af3
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2022-12-10 23:48:45 +0100

    submit username, make request link

--- index.js
+++ index.js
@@ -5,28 +5,10 @@ PlayerUserName = "";
 
 function SubmitUserName(){
     PlayerUserName = document.getElementById("usernameinput").value;
-    getHTTPrequest(MakeRequestLink(SUMMONER_INFO_REQUEST, EUNE, PlayerUserName));
-   // document.getElementById("summonericon").src = "https://ddragon.leagueoflegends.com/cdn/12.23.1/img/profileicon/"+ data.profileIconId +".png";
 }
 
 function MakeRequestLink(request_link, region, PlayerUserName){
     return "https://" + region + ".api.riotgames.com" + request_link + PlayerUserName+ "?api_key=" + API_KEY;
-    //console.log(request);
-}
-
-function getHTTPrequest(request){
-    const req = new XMLHttpRequest();
-    req.open("GET",request);
-    req.send();
-    req.responseType = "json";
-    req.onload = () => {
-        if (req.readyState == 4 && req.status == 200) {
-          const data = req.response;
-          console.log(data.profileIconId);
-        } else {
-          console.log(`Error: ${req.status}`);
-        }
-    }
 }
 
 
