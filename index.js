21523cab980a1d065cedb13a79510735ca7285b1
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2022-12-12 20:29:32 +0100

    .

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const API_KEY = "RGAPI-bde7d8c2-c271-439d-ac88-88c95b171267";
+const API_KEY = "RGAPI-ced84f47-dc25-467f-96a4-f78ffe01cfea";
 const SUMMONER_INFO_REQUEST = "/lol/summoner/v4/summoners/by-name/";
 
 function SubmitUserName(){
     let PlayerUserName = document.getElementById("usernameinput").value;
     let region =  GetRegion();
-    console.log(region);
     if(region != "" && PlayerUserName != ""){
         let url = MakeRequestLink(SUMMONER_INFO_REQUEST,region,PlayerUserName);
         let data = HTTPrequest("GET", url).then(data => {
