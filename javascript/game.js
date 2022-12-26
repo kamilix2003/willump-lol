7b6e076a44b7f16ad5a7fa5f1c51dea7fe468da3
Kamil Chaj <kamilchaj@student.agh.edu.pl>
2022-12-26 23:57:13 +0100

    display summoners, buttons to select summoner and display their stats

--- javascript/game.js
+++ javascript/game.js
@@ -7,24 +7,82 @@ let continent = "europe";
 const ctx = document.getElementById('myChart');
 
 let urlData = parseURLParams(window.location.href);
-let matchurl = `https://${continent}.api.riotgames.com/lol/match/v5/matches/${urlData.matchid}?api_key=${API_KEY}`
-let timelineurl = `https://${continent}.api.riotgames.com/lol/match/v5/matches/${urlData.matchid}/timeline?api_key=${API_KEY}`
+let matchurl = `https://${continent}.api.riotgames.com/lol/match/v5/matches/${urlData.matchid}?api_key=${API_KEY}`;
+let timelineurl = `https://${continent}.api.riotgames.com/lol/match/v5/matches/${urlData.matchid}/timeline?api_key=${API_KEY}`;
+
+document.getElementById("matchid").innerHTML += ` ${urlData.matchid}`;
 
 HTTPrequest("GET", matchurl).then(matchdata => {
+  let summoners = matchdata.info.participants;
+  console.log(summoners);
+  for(let i = 0; i < summoners.length; i++){
+    let summoner = NewElement(`
+    <div class="summoner" id="summoner-${i}">
+      <a class="summoner-link" id="summoner-link-${i}" href="results.html?region=EUN1&summonername=${summoners[i].summonerName}">
+        <img class="champion-img" id="champion-img-${i}" src="https://ddragon.leagueoflegends.com/cdn/img/champion/tiles/${summoners[i].championName}_0.jpg" alt="">
+        <p id="champion-name-${i}">${summoners[i].championName}</p>
+        <p id="summoner-name-${i}">${summoners[i].summonerName}</p>
+      </a>
+        <button id="summoner-stats-btn-${i}">🔥=></button>
+    </div>  
+    `);
+    let summonerContainer = document.querySelector(".match-summoners-container");
+    summonerContainer.appendChild(summoner);
+    document.querySelector(`#summoner-stats-btn-${i}`).addEventListener("click",(event) => {
+      let statsDiv = NewElement(`
+      <div class="summoner-stats-container">
+      <p id="test2">test2</p>
+      <p>kda: ${summoners[i].kills}/${summoners[i].deaths}/${summoners[i].assists}</p>
+      <p>dmg dealt: ${summoners[i].totalDamageDealtToChampions}</p>
+      <p>dmg taken: ${summoners[i].totalDamageTaken}</p>
+      <p>vision score: ${summoners[i].visionScore}</p>
+      <p>farm: ${summoners[i].totalMinionsKilled + summoners[i].neutralMinionsKilled}</p>
+      <div class="items">
+          <p>${summoners[i].item0}</p>
+          <p>${summoners[i].item1}</p>
+          <p>${summoners[i].item2}</p>
+          <p>${summoners[i].item3}</p>
+          <p>${summoners[i].item4}</p>
+          <p>${summoners[i].item5}</p>
+      </div>
+      <div class="runes">
+
+      </div>
+    </div>
+      `)
+      
+      
+      
+      document.querySelector(".summoner-stats-container").innerHTML = "";
+      document.querySelector(".summoner-stats-container").appendChild(statsDiv);
+
+    })
+  }
+
+
     HTTPrequest("GET", timelineurl).then(timeline =>{
         console.log("match data:", matchdata);
-        console.log("match timeline:",timeline);
+        // console.log("match timeline:",timeline);
 
-        let currentGold = getTimelineDataPlayers(timeline, "totalGold");
+        let currentGold = getTimelineDataPlayers(timeline, "currentGold");
+        let totalGold = getTimelineDataPlayers(timeline, "totalGold");
+        let kills = getTimelineDataPlayers(timeline, "xp")
 
-        let dataSet = makeChartDataSet(currentGold[1], "totalGold");
+        let dataSet2 = makeChartDataSet(totalGold[1], "totalGold")
+        let dataSet = makeChartDataSet(currentGold[1], "currentGold");
+        let dataSet3 = makeChartDataSet(kills[1], "xp")
         let labels = makeChartLabels(dataSet.data.length, 1, 1);
-        let chartData = makeChartData([dataSet], labels, "line", {});
-        console.log(chartData);
+        let chartData = makeChartData([dataSet, dataSet2], labels, "line", {});
+        let chartData2 = makeChartData([dataSet2, dataSet3], labels, "line", {});
         
         makeNewChartElement(".chart-container", "test1", chartData);
     })
   })
+
+function displaySummonerStats(summoners) {
+  document.getElementById("test2").innerHTML =
+    summoners[summonerNumber].championName;
+}
 
 function makeChartLabels(length, start, step){
   let labels = []
